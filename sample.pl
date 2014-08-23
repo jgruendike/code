cc633fa85438d41a6319bc1c652956ee015fa4a6
jgruendike <jgruendike@gmail.com>
2014-08-23 13:12:12 -0700

    Update sample.pl
    added new variable

--- sample.pl
+++ sample.pl
@@ -2,6 +2,7 @@
 use strict;
 
 my $limit = 100;
+my $var = 20;
 
 for(my $i = 0; $i <= $limit; $i++){
 	for(my $n = 0; $n < $i; $n++){
@@ -9,3 +10,5 @@ for(my $i = 0; $i <= $limit; $i++){
 	}
 	print $i."\n";
 }
+
+print ($var + 10) . "\n";
